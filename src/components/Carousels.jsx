2e19d46a5303ab857c7cb85f26856eaea7ed5ec9
LidiaKovac <lidia.kovac1998@gmail.com>
2020-11-20 19:58:53 +0100

    initial commit

---
+++ src/components/Carousels.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../netflix.css"
+import { Modal, Button, Form } from "react-bootstrap"
+
+class SingleCarousel extends React.Component {
+    render() {
+        let response = try {
+
+        } catch (error) {
+            console.log(error)
+        }
+        return (
+
+        
+
+    )
+    }
+
+
+} export default SingleCarousel
