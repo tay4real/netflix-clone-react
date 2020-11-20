823d5d94485296cb8c4cbd944f360e1170f3621c
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-11-20 14:42:14 +0100

    search logic implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-
-import './App.css';
-import Header from './components/header';
-import NetflixNavBar from './components/navbar';
+import "./App.css"
+import Header from "./components/header"
+import NetflixNavBar from "./components/navbar"
+import SearchBar from "./components/SearchBar"
 
 function App() {
+  const onChange = (e) => {
+    // when valuechanged
+    console.log(e.target.value)
+  }
+  const fetchMovies = async (query) => {
+    alert(query)
+  }
+  const onKeyDown = (e) => {
+    // when someone pressed any button
+    if (e.key === "Enter") {
+      e.preventDefault()
+      fetchMovies(e.target.value)
+    }
+  }
   return (
-    <> <NetflixNavBar />
+    <>
+      <NetflixNavBar onKeyDown={onKeyDown} onChange={onChange} />
       <Header />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,30 @@
-import React from "reac-app"
+import React from "react"
+import {
+  Alert,
+  Button,
+  Col,
+  Form,
+  Row,
+  Spinner,
+  FormControl,
+} from "react-bootstrap"
 
 class SearchBar extends React.Component {
-    state = {
+  state = {}
 
-    }
-
-    render () {
-        return (
-
-        )
-    }
+  render() {
+    return (
+      <Form inline>
+        <FormControl
+          type="text"
+          placeholder="Search"
+          onKeyDown={this.props.onKeyDown}
+          onChange={this.props.onChange}
+          className="mr-sm-2"
+        />
+        <Button variant="outline-danger"></Button>
+      </Form>
+    )
+  }
 }
 export default SearchBar
