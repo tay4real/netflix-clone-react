3dec488f5484579bbd7e22a4c82f00af82ff5a12
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-11-20 13:24:05 +0100

    Create SearchBar.jsx

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from "reac-app"
+
+class SearchBar extends React.Component {
+    state = {
+
+    }
+
+    render () {
+        return (
+
+        )
+    }
+}
+export default SearchBar
