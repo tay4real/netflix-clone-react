6b318a59f66e75e9099304f4d525c2c619561b9f
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-11-21 23:41:36 +0100

    displays search result in Results

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,49 @@
+import React from "react"
 import "./App.css"
 import CommentList from "./components/CommentList"
 import Header from "./components/Header"
 import Results from "./components/Results"
 import NetflixNavBar from "./components/Navbar"
+import {render} from "@testing-library/react"
 
-function App() {
-  const onChange = (e) => {
+class App extends React.Component {
+  state = {
+    res_: "",
+  }
+
+  onChange = (e) => {
     // when valuechanged
     console.log(e.target.value)
   }
-  const fetchMovies = async (query) => {
+  fetchMovies = async (query) => {
     let response = await fetch(
       `http://www.omdbapi.com/?t=${query}&apikey=a0871843&`
     )
     let result = await response.json()
-    console.log(result)
-    return result
+    let res = await result.Title
+    console.log(res)
+    this.setState({res_: res})
   }
 
-  const onKeyDown = (e) => {
+  onKeyDown = (e) => {
     // when someone pressed any button
     if (e.key === "Enter") {
       e.preventDefault()
-      fetchMovies(e.target.value)
+      this.fetchMovies(e.target.value)
     }
   }
-  return (
-    <>
-      {" "}
-      <NetflixNavBar onKeyDown={onKeyDown} onChange={onChange} />
-      <Header />
-      <Results results={this.fetchMovies} />
-      <CommentList />
-    </>
-  )
+
+  render() {
+    return (
+      <>
+        {" "}
+        <NetflixNavBar onKeyDown={this.onKeyDown} onChange={this.onChange} />
+        <Header />
+        <Results results={this.state.res_} />
+        <CommentList />
+      </>
+    )
+  }
 }
 
 export default App
