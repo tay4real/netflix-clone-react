66281e60a582fb029ff3d058c2044cc6a0b478cb
Ademuyiwa Otubusin <tay4real@gmail.com>
2020-11-22 22:50:49 +0100

    removed import bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { Col, Modal, Row } from "react-bootstrap"
 import "../netflix.css"
 import video from "../assets/lietomecut.mp4"
